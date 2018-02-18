c4e29bb3c5cd4b7b00a2a911373931a2854dc67d
Adam Lochno <adamlochno@gmail.com>
2018-02-18 12:42:42 +0100

    first commit

---
+++ Kantor_20.java
@@ -0,0 +1,37 @@
+
+package kantor_2.pkg0;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Kantor_20 {
+
+   public static int wartosc1;
+   public static String waluta1,waluta2;
+    
+    
+    public static void main(String[] args) throws IOException 
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        System.out.println("Wprowadź wartość do obliczeń:");
+        wartosc1 = Integer.parseInt(br.readLine());
+        
+        System.out.println("Wprowadź waluta1 ZLOTY,EURO,DOLLAR,FUNT,FRANK:");
+        waluta1 = br.readLine();
+        
+        System.out.println("Wprowadź waluta2 ZLOTY,EURO,DOLLAR,FUNT,FRANK:");
+        waluta2 = br.readLine();
+        
+        
+        System.out.println("a:"+wartosc1 +" waluta1:"+waluta1+" waluta2:"+waluta2);
+        
+        Obliczenia kantor= new Obliczenia();
+        kantor.obliczenia();
+        kantor.wyswietlWynik();
+        
+    }
+    
+}
