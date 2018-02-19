e04fa0228f271b5083eb403c40630a878800d551
Adam Lochno <adamlochno@gmail.com>
2018-02-19 21:23:00 +0100

    another

--- Kantor_20.java
+++ Kantor_20.java
@@ -7,39 +7,38 @@ import java.io.InputStreamReader;
 
 
 public class Kantor_20 {
-
-   public static int wartosc1;
-   public static String waluta1,waluta2;
-    
     
     public static void main(String[] args) throws IOException 
     {
-        //WPROWADZENIE WARTOŚCI
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        System.out.println("Wprowadź wartość do obliczeń:");
-        wartosc1 = Integer.parseInt(br.readLine());
-        
-        System.out.println("Wprowadź waluta1 ZLOTY,EURO,DOLLAR,FUNT,FRANK:");
-        waluta1 = br.readLine();
-        
-        System.out.println("Wprowadź waluta2 ZLOTY,EURO,DOLLAR,FUNT,FRANK:");
-        waluta2 = br.readLine();
-        
-        //ZMIANA WARTOŚCI NA MAŁE ZNAKI       
-        waluta1 = waluta1.replaceAll("[^a-zA-Z]+","").toLowerCase();
-        waluta2 = waluta2.replaceAll("[^a-zA-Z]+","").toLowerCase();
-        
-        System.out.println("a:"+wartosc1 +" waluta1:"+waluta1+" waluta2:"+waluta2);
+//        //WPROWADZENIE WARTOŚCI
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        
+//        System.out.println("Wprowadź wartość do obliczeń:");
+//        wartosc1 = Integer.parseInt(br.readLine());
+//        
+//        System.out.println("Wprowadź waluta1 ZLOTY,EURO,DOLLAR,FUNT,FRANK:");
+//        waluta1 = br.readLine();
+//        
+//        System.out.println("Wprowadź waluta2 ZLOTY,EURO,DOLLAR,FUNT,FRANK:");
+//        waluta2 = br.readLine();
+//        
+//        //ZMIANA WARTOŚCI NA MAŁE ZNAKI       
+//        waluta1 = waluta1.replaceAll("[^a-zA-Z]+","").toLowerCase();
+//        waluta2 = waluta2.replaceAll("[^a-zA-Z]+","").toLowerCase();
+//        
+//        System.out.println("a:"+wartosc1 +" waluta1:"+waluta1+" waluta2:"+waluta2);
         
         Obliczenia kantor= new Obliczenia();
-        kantor.obliczenia();
-        kantor.wyswietlWynik();
+        //kantor.obliczenia();
+        //kantor.wyswietlWynik();
         PullData pulldata = new PullData();        
         pulldata.getRateEuro();
         pulldata.getRateDollar();
         pulldata.getRateFunt();
         pulldata.getRateFrank();
+        pulldata.Druk();
+        
+        
     }
     
 }
