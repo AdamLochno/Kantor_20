c88d8f7e07955ec9291664085e773667a606c741
Kuba Bieda <bieda.kuba@gmail.com>
2018-02-20 22:11:15 +0100

    formatting fix

--- src/main/java/kantor/PushData.java
+++ src/main/java/kantor/PushData.java
@@ -6,31 +6,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 
-
-public class PushData 
-
-{
+public class PushData {
     public int wartosc1;
-    public String waluta1,waluta2;
+    public String waluta1, waluta2;
     private BufferedReader reader;
 
-    public void PushData() throws IOException
-    {
+    public void PushData() throws IOException {
         reader = new BufferedReader(new InputStreamReader(System.in));
-        
+
         System.out.println("Wprowadź wartość do obliczeń:");
         wartosc1 = Integer.parseInt(reader.readLine());
-        
+
         System.out.println("Wybierz walutę początkową ZLOTY,EURO,DOLLAR,FUNT,FRANK:");
         waluta1 = reader.readLine();
-        
+
         System.out.println("Wybierz walutę docelową ZLOTY,EURO,DOLLAR,FUNT,FRANK:");
         waluta2 = reader.readLine();
-        
+
         //ZMIANA WARTOŚCI NA MAŁE ZNAKI       
-        waluta1 = waluta1.replaceAll("[^a-zA-Z]+","").toLowerCase();
-        waluta2 = waluta2.replaceAll("[^a-zA-Z]+","").toLowerCase();
-        
-        System.out.println("a:"+wartosc1 +" waluta1:"+waluta1+" waluta2:"+waluta2);   
+        waluta1 = waluta1.replaceAll("[^a-zA-Z]+", "").toLowerCase();
+        waluta2 = waluta2.replaceAll("[^a-zA-Z]+", "").toLowerCase();
+
+        System.out.println("a:" + wartosc1 + " waluta1:" + waluta1 + " waluta2:" + waluta2);
     }
 }
