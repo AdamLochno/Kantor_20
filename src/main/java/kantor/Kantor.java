ef99fa28f475818a8385a5d0ef318adbd3937299
Kuba Bieda <bieda.kuba@gmail.com>
2018-02-20 21:56:29 +0100

    Kantor cleanup

--- src/main/java/kantor/Kantor.java
+++ src/main/java/kantor/Kantor.java
@@ -5,15 +5,16 @@ import java.io.IOException;
 
 
 public class Kantor {
-    public int wartosc1;
-    public String waluta1, waluta2;
 
     public static void main(String[] args) throws IOException {
-        //WPROWADZAMY WARTOŚCI
+        Kantor kantor = new Kantor();
+        kantor.run();
+    }
+
+    private void run() throws IOException {
         PushData pushdata = new PushData();
         pushdata.PushData();
 
-        //ZACIĄGAMY DANE ODNOSNIE WALUT
         PullData pulldata = new PullData();
         pulldata.getRateEuro();
         pulldata.getRateDollar();
@@ -21,11 +22,9 @@ public class Kantor {
         pulldata.getRateFrank();
         pulldata.Druk();
 
-        //PROGRAM LICZY
         Obliczenia kantor = new Obliczenia();
         kantor.obliczenia();
 
-        //PROGRAM WYŚWIETLA WYNIKI
         kantor.wyswietlWynik();
     }
 
