cbc5ed008a2d9060f44d318d2b6c152cc35285cc
Adam <34002561+AdamLochno@users.noreply.github.com>
2018-02-25 20:00:40 +0100

    Add files via upload

--- src/main/java/kantor/Obliczenia.java
+++ src/main/java/kantor/Obliczenia.java
@@ -1,121 +1,123 @@
 
 package kantor;
 
+import java.io.IOException;
+
 
 public class Obliczenia 
 {
     private double zloty = 1;
-    private double euro = 4.3;
+    private double euro = 4.6;
     private double dollar = 3.8;
     private double funt = 4.9;
     private double frankSzwajcarski = 3.6;
+    
 
-    public double oblicz(UserInput userInput) 
+    public double obliczenia(UserInput userInput) 
     {
         return oblicz(userInput.getAmount(), userInput.getInitialCurrency(), userInput.getTargetCurrency());
     }   
-    
-   public double TakeData(PullData pullData)
-   {
-      //PullData pullData = new PullData();
-      return TakeData (pullData.getRateEuro());
-   }
-
-    public double oblicz(int wartosc1, String waluta1, String waluta2) 
+      
+    private PullData pullData = new PullData();
+         
+    public double oblicz(int Amount, String InitialCurrency, String TargetCurrency) 
     {
         double wynik = 0;
-        if ("zloty".equals(waluta1)) {
-            if ("zloty".equals(waluta2)) {
-                wynik = wartosc1 / zloty;
+//        euro = valueEuro;
+        if ("zloty".equals(InitialCurrency)) {
+            if ("zloty".equals(TargetCurrency)) {
+                wynik = Amount / zloty;
             }
-            if ("euro".equals(waluta2)) {
-                wynik = wartosc1 / euro;
+            if ("euro".equals(TargetCurrency)) 
+            {
+                pullData.getRateEuro();
+                wynik = Amount /valueEuro;
             }
-            if ("dollar".equals(waluta2)) {
-                wynik = wartosc1 / dollar;
+            if ("dollar".equals(TargetCurrency)) {
+                wynik = Amount / dollar;
             }
-            if ("funt".equals(waluta2)) {
-                wynik = wartosc1 / funt;
+            if ("funt".equals(TargetCurrency)) {
+                wynik = Amount / funt;
             }
-            if ("frankSzwajcarski".equals(waluta2)) {
-                wynik = wartosc1 / frankSzwajcarski;
+            if ("frankSzwajcarski".equals(TargetCurrency)) {
+                wynik = Amount / frankSzwajcarski;
             }
         }
 
         //WARTOŚĆ WEJŚCIOWA JEST W EURO
-        if ("euro".equals(waluta1)) {
-            wartosc1 = (int) (wartosc1 * euro); //OTRZYMUJEMY WARTOŚĆ WEJŚCIOWĄ W ZŁOTÓWKACH
-            if ("zloty".equals(waluta2)) {
-                wynik = wartosc1 / zloty;
+        if ("euro".equals(InitialCurrency)) {
+            Amount = (int) (Amount * euro); //OTRZYMUJEMY WARTOŚĆ WEJŚCIOWĄ W ZŁOTÓWKACH
+            if ("zloty".equals(TargetCurrency)) {
+                wynik = Amount / zloty;
             }
-            if ("euro".equals(waluta2)) {
-                wynik = wartosc1 / euro;
+            if ("euro".equals(TargetCurrency)) {
+                wynik = Amount / euro;
             }
-            if ("dollar".equals(waluta2)) {
-                wynik = wartosc1 / dollar;
+            if ("dollar".equals(TargetCurrency)) {
+                wynik = Amount / dollar;
             }
-            if ("funt".equals(waluta2)) {
-                wynik = wartosc1 / funt;
+            if ("funt".equals(TargetCurrency)) {
+                wynik = Amount / funt;
             }
-            if ("frankSzwajcarski".equals(waluta2)) {
-                wynik = wartosc1 / frankSzwajcarski;
+            if ("frankSzwajcarski".equals(TargetCurrency)) {
+                wynik = Amount / frankSzwajcarski;
             }
         }
         //WARTOŚĆ WEJŚCIOWA JEST W DOLLARACH
-        if ("dollar".equals(waluta1)) {
-            wartosc1 = (int) (wartosc1 * dollar); //OTRZYMUJEMY WARTOŚĆ WEJŚCIOWĄ W ZŁOTÓWKACH
-            if ("zloty".equals(waluta2)) {
-                wynik = wartosc1 / zloty;
+        if ("dollar".equals(InitialCurrency)) {
+            Amount = (int) (Amount * dollar); //OTRZYMUJEMY WARTOŚĆ WEJŚCIOWĄ W ZŁOTÓWKACH
+            if ("zloty".equals(TargetCurrency)) {
+                wynik = Amount / zloty;
             }
-            if ("euro".equals(waluta2)) {
-                wynik = wartosc1 / euro;
+            if ("euro".equals(TargetCurrency)) {
+                wynik = Amount / euro;
             }
-            if ("dollar".equals(waluta2)) {
-                wynik = wartosc1 / dollar;
+            if ("dollar".equals(TargetCurrency)) {
+                wynik = Amount / dollar;
             }
-            if ("funt".equals(waluta2)) {
-                wynik = wartosc1 / funt;
+            if ("funt".equals(TargetCurrency)) {
+                wynik = Amount / funt;
             }
-            if ("frankSzwajcarski".equals(waluta2)) {
-                wynik = wartosc1 / frankSzwajcarski;
+            if ("frankSzwajcarski".equals(TargetCurrency)) {
+                wynik = Amount / frankSzwajcarski;
             }
         }
         //WARTOŚĆ WEJŚCIOWA JEST W FUNTACH
-        if ("funt".equals(waluta1)) {
-            wartosc1 = (int) (wartosc1 * euro); //OTRZYMUJEMY WARTOŚĆ WEJŚCIOWĄ W ZŁOTÓWKACH
-            if ("zloty".equals(waluta2)) {
-                wynik = wartosc1 / zloty;
+        if ("funt".equals(InitialCurrency)) {
+            Amount = (int) (Amount * euro); //OTRZYMUJEMY WARTOŚĆ WEJŚCIOWĄ W ZŁOTÓWKACH
+            if ("zloty".equals(TargetCurrency)) {
+                wynik = Amount / zloty;
             }
-            if ("euro".equals(waluta2)) {
-                wynik = wartosc1 / euro;
+            if ("euro".equals(TargetCurrency)) {
+                wynik = Amount / euro;
             }
-            if ("dollar".equals(waluta2)) {
-                wynik = wartosc1 / dollar;
+            if ("dollar".equals(TargetCurrency)) {
+                wynik = Amount / dollar;
             }
-            if ("funt".equals(waluta2)) {
-                wynik = wartosc1 / funt;
+            if ("funt".equals(TargetCurrency)) {
+                wynik = Amount / funt;
             }
-            if ("frankSzwajcarski".equals(waluta2)) {
-                wynik = wartosc1 / frankSzwajcarski;
+            if ("frankSzwajcarski".equals(TargetCurrency)) {
+                wynik = Amount / frankSzwajcarski;
             }
         }
         //WARTOŚĆ WEJŚCIOWA JEST WE FRANKACH
-        if ("frank".equals(waluta1)) {
-            wartosc1 = (int) (wartosc1 * euro); //OTRZYMUJEMY WARTOŚĆ WEJŚCIOWĄ W ZŁOTÓWKACH
-            if ("zloty".equals(waluta2)) {
-                wynik = wartosc1 / zloty;
+        if ("frank".equals(InitialCurrency)) {
+            Amount = (int) (Amount * euro); //OTRZYMUJEMY WARTOŚĆ WEJŚCIOWĄ W ZŁOTÓWKACH
+            if ("zloty".equals(TargetCurrency)) {
+                wynik = Amount / zloty;
             }
-            if ("euro".equals(waluta2)) {
-                wynik = wartosc1 / euro;
+            if ("euro".equals(TargetCurrency)) {
+                wynik = Amount / euro;
             }
-            if ("dollar".equals(waluta2)) {
-                wynik = wartosc1 / dollar;
+            if ("dollar".equals(TargetCurrency)) {
+                wynik = Amount / dollar;
             }
-            if ("funt".equals(waluta2)) {
-                wynik = wartosc1 / funt;
+            if ("funt".equals(TargetCurrency)) {
+                wynik = Amount / funt;
             }
-            if ("frankSzwajcarski".equals(waluta2)) {
-                wynik = wartosc1 / frankSzwajcarski;
+            if ("frankSzwajcarski".equals(TargetCurrency)) {
+                wynik = Amount / frankSzwajcarski;
             }
         }
         return wynik;
